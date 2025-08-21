bbccee917780883c8b440f7c592ac886e273c839
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-21 12:41:23 +0530

    added check for root folder

--- src/config_manager.rs
+++ src/config_manager.rs
@@ -1,4 +1,17 @@
-use std::fs;
+use std::{fs, path};
+use std::path::Path;
+
+const CONFIG_ROOT: &str = "%APPDATA%/AutoCrate/";
+pub fn is_there_config() -> bool{
+    let root_path = Path::new(CONFIG_ROOT);
+    let is_root_exist = if root_path.exists() 
+    { 
+        true
+    } else {
+        false
+    };
+    return is_root_exist;
+}
 
 pub fn test(){
     println!("Testing Sucess!");
