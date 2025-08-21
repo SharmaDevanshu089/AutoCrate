56578a4c7c42881b2bd715ebb8b5da32b6b1d1b5
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-21 11:40:20 +0530

    Infinite Writer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-mod file;
-
+//mod file;
+use std::io::Write;
+use std::fs::{self, File, OpenOptions};
+use std::path::Path;
 fn main() {
-    file::hello();
+    let Count = Path::new("Count.txt");
+    if Count.exists(){
+        fnloop();
+    }
+    else {
+        let newfile = File::create("Count.txt").unwrap();
+    }
+}
+fn fnloop() {
+    let mut i :i128 = 0;
+    let mut file = OpenOptions::new().append(true).open("Count.txt").unwrap();
+    while i > -1 {
+    writeln!(file,"{}",i).unwrap();
+    i = i+1;
+    }
 }
