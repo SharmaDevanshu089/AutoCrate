c5bfd233a11c2b3b4627e8fc5c6d19d1934e162b
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-22 22:38:50 +0530

    Updated to v0.4.7 and fixed the config saving error added directories as dependency

--- src/config_manager.rs
+++ src/config_manager.rs
@@ -1,14 +1,16 @@
 use std::fs::{create_dir_all, File};
 //use std::{fs, path};
 use std::path::Path;
+use directories::ProjectDirs;
 
 use crate::{error_handler, first_run};
 
-const CONFIG_ROOT: &str = "DATA";
-const CONFIG_FILE: &str = "DATA/_Config.json";
+const CONFIG_ROOT: &str = "DATA";//for if some code stopped working
+const CONFIG_FILE: &str = "DATA/_Config.json";//want to replace these with get_directory
+const SERIOUS_ERROR: &str = "A very Serious Internal Compilation time error occured cannot continue, try recompling form source";//yeah i want my code clean
 
 pub fn is_there_config() -> bool{
-    let root_path = Path::new(CONFIG_ROOT);
+    let root_path = Path::new(get_directory("config_root"));
     let is_root_exist = if root_path.exists() 
     { 
         true
@@ -26,16 +28,29 @@ pub fn get_config(){
     }
 }
 pub fn create_root() {
-    let _new_root = create_dir_all(CONFIG_ROOT);
+    let _new_root = create_dir_all(get_directory("config_root"));
     match _new_root {
         Ok(_new_root) => (),
         Err(_new_root) => error_handler::errorout("dir_creation",_new_root.to_string()),
     }
-    let _new_file = File::create(CONFIG_FILE);
+    let _new_file = File::create(get_directory("config_file"));
     match  _new_file{
         Ok(_new_file) => (),
         Err(_new_file) => error_handler::errorout("config_creation",_new_file.to_string()),
     }
     //Config Creation Sucess but this is first run
     first_run::setup();
+}
+pub fn get_directory(type_of:&str) -> String{
+    let program_name = ProjectDirs::from("dev", "sharmadevanshu089", "autocrate").expect(SERIOUS_ERROR);
+    let config_root = program_name.config_dir().to_str().expect(SERIOUS_ERROR);
+    let config_file_old = config_root.to_owned()+"\\_Config.json";
+    let config_file = config_file_old.as_str();
+    let mut return_value = "";
+    match type_of {
+        "config_file" => return_value = config_file,
+        "config_root" => return_value = config_root,
+        _ => error_handler::errorout_no_log("unmentioned_type_of"),
+    }
+    return return_value.to_owned();
 }
