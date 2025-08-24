74a8846d0171446596b6e039bdb76c5eaa917334
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-25 00:51:57 +0530

    Existential crysis at midnight

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -1,6 +1,8 @@
 use std::fs::OpenOptions;
 use std::io::Write;
-use crate::first_run;
+use colored::Colorize;
+
+use crate::{first_run::{self, get_program_name_to_json}, main};
 
 const LOGFILE:&str = "autocrate_debug.log"; 
 
@@ -9,7 +11,9 @@ pub fn errorout(error: &str, to_log:String) {
         "config_creation" => { println!("Unable to Create Config;");log(to_log);}
         "dir_creation" => { println!("Unable to Create Root Directory;");log(to_log);}
         "need_string_valid" => {println!("Need Valid String");first_run::get_program_name_to_json();}
-        _ => {println!("Unkown Error Occured");}
+        "name_is_empty" => {println!("The String should not be empty");first_run::get_program_name_to_json();}
+        "regex_error" => {println!("Unable to Create a Filter. This is a fatal error. Please check github for latest Release. Restarting Program");main();log(to_log);}
+        _ => {println!("Unkown Error Occured");panic!("Error Occured")}
     }
 }
 fn log(to_log:String) {
@@ -22,4 +26,10 @@ pub fn errorout_no_log(error: &str) {
         "unmentioned_type_of" => { println!("The Program didnt mention the type of directory to fetch;");log(to_log);}
         _ => {println!("Unkown Error Occured;");}
     }
+}
+pub fn not_valid_string(){
+    let error_msg = "Name Can only be a legal foldername and only can contain ".color("red");
+    let highlight = "Alphabets (A-Z,a-z), Number (0-9)".bold().color("yellow");
+    println!("{} {}",error_msg,highlight);
+    get_program_name_to_json();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use regex::Regex;
 
 mod config_manager;
 mod error_handler;
@@ -11,5 +12,19 @@ pub fn get_input() -> String {
     match line_read {
         Ok(line_read) => line_read,
         Err(line_read) => {error_handler::errorout("need_string_valid", line_read.to_string());unreachable!()},
+    }
+}
+pub fn filter(data:String) -> String{
+    let regex_filter=Regex::new("^[a-zA-Z0-9_-]+$")/* .unwrap_or_else(|_| error_handler::errorout("regex_error", regex_filter.to_string())) */;
+    match regex_filter {
+         Ok(regex_filter) => regex_filter,
+         Err(regex_filter) => error_handler::errorout("regex_error", regex_filter.to_string()),
+    }
+    if regex_filter.is_match(data) {
+        return data;
+    }
+    else {
+        error_handler::not_valid_string();
+        return "err".to_owned();
     }
 }
